00166627743171d5a2032202dfda3e89634a833b
abhishek <kaushikabhishek87@gmail.com>
2022-05-19 19:46:23 +0530

    intial commit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+_=""" Day 2 """
+# st.write("Hello World")
+
+_="""Day 3"""
+
+
+st.magicEnabled = False
+st.header('st.button')
+
+if st.button('Say Hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
